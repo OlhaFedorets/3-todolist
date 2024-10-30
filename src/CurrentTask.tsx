bbb6136ae8e7801624f9a0e887e3e6d2bf0a4d4d
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-30 17:36:23 +0300

    change currentTask

--- src/CurrentTask.tsx
+++ src/CurrentTask.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import {TaskType} from "./App";
+import {v1} from "uuid";
 
 type CurrentTaskType = {
-
+    removeTaskHandler:()=>void
+    isDone: boolean
+    title: string
 }
 
-export const CurrentTask = () => {
+export const CurrentTask = ({removeTaskHandler, isDone, title}: CurrentTaskType) => {
     return (
         <>
-            <input type="checkbox" checked={task.isDone}/>
-            <span>{task.title}</span>
+            <input type="checkbox" checked={isDone}/>
+            <span>{title}</span>
             {/*<Button title={'x'} onClick={() => removeTask(task.id)}/>*/}
             <button onClick={removeTaskHandler}>x</button>
         </>
